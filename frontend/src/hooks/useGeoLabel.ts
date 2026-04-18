22c5f49e5a3042537d9730b4a5729df862f9f075
lyangjyehaur <lyangjyehaur@gmail.com>
2026-04-18 11:10:59 +0800

    補充Geo相關

--- frontend/src/hooks/useGeoLabel.ts
+++ frontend/src/hooks/useGeoLabel.ts
@@ -19,16 +19,16 @@ export function useGeoLabel() {
       if (info.isVPN) {
         setGeoInfo({
           labelCn: "躍遷模式",
-          labelEn: "WARP_DRIVE",
-          desc: "偵測到空間躍遷訊號 🛸！既然您已具備跨越網路結界的能力，系統已自動為您解除代理限制。圖片將直接從海外官方 CDN 載入，絕不浪費一滴伺服器頻寬！"
+          labelEn: "WARP",
+          desc: "偵測到空間躍遷訊號 🛸！既然您已具備跨越網路結界的能力，圖片資源將直接從 X/YouTube 官方 CDN 載入，就不浪費代理伺服器頻寬啦！"
         });
       } else {
         setGeoInfo({
           labelCn: info.isChinaIP ? "境內節點" : "國際邊緣",
-          labelEn: info.isChinaIP ? "CN_NODE" : "GLOBAL_EDGE",
+          labelEn: info.isChinaIP ? "CN" : "GLOBAL",
           desc: info.isChinaIP 
-            ? "偵測到您正處於境內網路，已自動切換至「香港直連專線」。圖片資源將經由 ZTMY 專屬代理伺服器加速，為您提供穩定、流暢的看圖體驗。"
-            : "偵測到您位於海外地區，已為您分配「全球邊緣節點」。圖片資源將直接自官方伺服器 (Twitter/YouTube) 載入，享受物理極限速度！"
+            ? "偵測到您正位於中國大陸，圖片資源將經由代理伺服器加速，為您提供穩定、流暢的看圖體驗。"
+            : "偵測到您位於中國大陸以外地區，圖片資源將直接自官方伺服器 (X/YouTube) 載入，享受物理極限速度！"
         });
       }
     });
